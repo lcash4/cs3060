91a77455985c5413ea56f42366638a978f87411a
lcash4 <lcash@bgsu.edu>
2025-11-21 13:52:27 -0500

    Add files via upload

---
+++ NumGame.scala
@@ -0,0 +1,23 @@
+import scala.io.StdIn._
+import scala.util.Random
+
+object NumGame {
+    def main(args: Array[String]):Unit = {
+
+        var found = false
+        val randomNum = Random.nextInt(101)
+        while found == false do {
+            val usernum = readLine("Enter a number between 0 and 100: ").toInt
+            println(s"You entered: $usernum")
+
+            if (usernum < randomNum) {
+                println("Too low! Try again.")
+            } else if (usernum > randomNum) {
+                println("Too high! Try again.")
+            } else {
+                println("Congratulations! You guessed the number!")
+                found = true
+            }
+        }
+    }
+}
